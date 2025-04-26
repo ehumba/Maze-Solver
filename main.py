bfe7ce81bf9fc6f570f8416836725885adec13cb
ehumba <behsukiat@gmail.com>
2025-04-26 15:44:15 +0800

    point and line

--- main.py
+++ main.py
@@ -20,15 +20,39 @@ class Window:
             self.redraw()
     def close(self):
         self.__running = False
+    def draw_line(self, line, fill_colour):
+        line.draw(self.__canvas, fill_colour)
+
 
 class Point:
     def __init__(self, x, y):
-        pass
+        self.x = x
+        self.y = y
 
+class Line:
+    def __init__(self, point1, point2):
+        self.__point1 = point1
+        self.__point2 = point2
+    def draw(self, canvas, fill_colour):
+        x1, y1 = self.__point1.x, self.__point1.y
+        x2, y2 = self.__point2.x, self.__point2.y
+        canvas.create_line(
+        x1, y1, x2, y2, fill=fill_colour, width=2
+)
+
+class Cell:
+    def __init__(self):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = 
+        
 
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(89, 104), Point(55, 104)), "black")
     win.wait_for_close()
 
 main()
